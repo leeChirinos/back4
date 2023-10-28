6e73ab8c529bc75e865c1acf407b507176012bd5
GabrielApaza <66227511+GabrielApaza@users.noreply.github.com>
2023-10-28 18:00:00 -0300

    proyecto

---
+++ copol/src/main/java/com/example/copol/CopolApplication.java
@@ -0,0 +1,13 @@
+package com.example.copol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopolApplication.class, args);
+	}
+
+}
---
+++ copol/src/test/java/com/example/copol/CopolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
