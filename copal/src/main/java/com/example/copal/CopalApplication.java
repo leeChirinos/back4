0a80e85d2073a07fc7e6d712f3aa7d303e2272f6
GabrielApaza <66227511+GabrielApaza@users.noreply.github.com>
2023-10-28 21:44:08 -0300

    copal up

---
+++ copal/src/main/java/com/example/copal/CopalApplication.java
@@ -0,0 +1,13 @@
+package com.example.copal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopalApplication.class, args);
+	}
+
+}
